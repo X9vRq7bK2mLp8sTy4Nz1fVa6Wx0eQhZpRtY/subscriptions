b4fa62c4d5a32e94acbadea3c38d1e7d081627da
AMIXNSUJNJNSIXNSIHXNSUHBXEUHBUHNKSJN <jawshcx@icloud.com>
2025-08-31 02:26:13 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', event => {
     const data = event.data.json();
     self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: '/icon.png' // Optional: Add an icon to your public folder
+        body: data.body
+        // icon: '/icon.png' // Uncomment if icon.png exists in public folder
     });
 });
